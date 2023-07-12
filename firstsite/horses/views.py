bc0b4d94fbd5e1efed95755c94b54c7cfb3e240d
macloody <139360325+macloody@users.noreply.github.com>
2023-07-12 16:52:44 +0300

    Add files via upload

---
+++ firstsite/horses/views.py
@@ -0,0 +1,182 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import *
+from .models import *
+from .utils import *
+
+
+class HorsesHome(DataMixin, ListView):
+    model = Horses
+    template_name = 'horses/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница')
+        return context | c_def  # dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        return Horses.objects.filter(is_published=True).select_related('cat')
+
+# def index(request):  # HttpRequest
+#     posts = Horses.objects.all()
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'cat_selected': 0
+#     }
+#     return render(request, 'horses/index.html', context)
+
+
+def about(request):
+    contact_list = Horses.objects.all()
+    paginator = Paginator(contact_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'horses/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
+
+
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'horses/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Добавление статьи')
+        return context | c_def
+
+# def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'horses/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+
+
+# def contact(request):
+#     return HttpResponse('Обратная связь')
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'horses/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная связь')
+        return context | c_def
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+
+# def login(request):
+#     return HttpResponse('Авторизация')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound(f'<h1>Страница не найдена</h1>')
+
+
+class ShowPost(DataMixin, DetailView):
+    model = Horses
+    template_name = 'horses/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return context | c_def
+
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Horses, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'horses/post.html', context=context)
+
+
+class HorsesCategory(DataMixin, ListView):
+    model = Horses
+    template_name = 'horses/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Horses.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name),
+                                      cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
+
+# def show_category(request, cat_slug):
+#     cat = Category.objects.get(slug=cat_slug)
+#     posts = Horses.objects.filter(cat_id=cat.pk)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Отображение по рубрикам',
+#         'cat_selected': cat.pk,
+#
+#     }
+#     return render(request, 'horses/index.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'horses/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return context | c_def
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'horses/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return context | c_def
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
